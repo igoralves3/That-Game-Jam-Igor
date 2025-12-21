4437df8accd3e09e0e6fffc1cef043dd96819ffe
igoralves3 <80365559+igoralves3@users.noreply.github.com>
2025-12-21 13:45:38 -0300

    scrips e cenas para construções

---
+++ thatgamejam/scripts/Buildings/alojamento.gd
@@ -0,0 +1,5 @@
+extends CharacterBody2D
+
+var total_construido = 1
+const max_seguidores = 3
+var custo_madeira = 50
---
+++ thatgamejam/scripts/Buildings/madeireira.gd
@@ -0,0 +1,5 @@
+extends CharacterBody2D
+
+var total_construido = 0
+const capacidade_maxima = 10
+var custo_madeira = 100
