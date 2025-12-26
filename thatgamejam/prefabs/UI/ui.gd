78ed93e277a84d9e1387ab327c713610223f71c2
JoaoPioner <51408829+JoaoPioner@users.noreply.github.com>
2025-12-26 19:59:14 -0300

    Mockup da UI criado

--- thatgamejam/prefabs/UI/ui.gd
+++ thatgamejam/prefabs/UI/ui.gd
@@ -1,15 +1,15 @@
 extends Control
 
-@onready var weekday_label = $BottomPanel/Controller/TimeController/MarginContainer/HBoxContainer/Weekday
-@onready var pause_button = $BottomPanel/Controller/TimeController/MarginContainer/HBoxContainer/Pause
-@onready var speed_1x_button = $"BottomPanel/Controller/TimeController/MarginContainer/HBoxContainer/HBoxContainer/1x"
-@onready var speed_2x_button = $"BottomPanel/Controller/TimeController/MarginContainer/HBoxContainer/HBoxContainer/2x"
+@onready var weekday_label = $Controller/TimeController/MarginContainer/HBoxContainer/Weekday
+@onready var pause_button = $Controller/TimeController/MarginContainer/HBoxContainer/Pause
+@onready var speed_1x_button = $"Controller/TimeController/MarginContainer/HBoxContainer/HBoxContainer/1x"
+@onready var speed_2x_button = $"Controller/TimeController/MarginContainer/HBoxContainer/HBoxContainer/2x"
 
-@onready var time_label = $BottomPanel/Controller/TimeController/MarginContainer/HBoxContainer/Time
+@onready var time_label = $Controller/TimeController/MarginContainer/HBoxContainer/Time
 
-@onready var wood_count_label = $TopPanel/MarginContainer/TopTab/WoodCounter/MarginContainer/HBox/Label
-@onready var supply_count_label = $TopPanel/MarginContainer/TopTab/SupplyCounter/MarginContainer/HBox/Label
-@onready var money_count_label = $TopPanel/MarginContainer/TopTab/CoinCounter/MarginContainer/HBox/Label
+@onready var wood_count_label = $TopPanel/MarginContainer/TopTab/WoodCounter/HBox/Label
+@onready var supply_count_label = $TopPanel/MarginContainer/TopTab/SupplyCounter/HBox/Label
+@onready var money_count_label = $TopPanel/MarginContainer/TopTab/GoldCounter/HBox/Label
 
 func _ready():
 	process_mode = Node.PROCESS_MODE_ALWAYS
@@ -34,18 +34,30 @@ func _on_pause_pressed():
 		timer.pause()
 
 func _on_1x_pressed():
+	if timer.paused:
+		Global.resume_game()
+		timer.resume()
 	timer.set_speed(1.0)
 
 func _on_2x_pressed():
+	if timer.paused:
+		Global.resume_game()
+		timer.resume()
 	timer.set_speed(2.0)
 
 
 func _update_pause_button(is_paused):
-	pause_button.text = "Play" if is_paused else "Pause"
+	#pause_button.text = "Play" if is_paused else "Pause"
+	pass
 
 func _update_speed_buttons(current_speed):
-	speed_1x_button.disabled = current_speed == 1.0
-	speed_2x_button.disabled = current_speed == 2.0
+	pass
+	#speed_1x_button.disabled = current_speed == 1.0
+	#speed_2x_button.disabled = current_speed == 2.0
 
 func _update_weekday(day, week):
 	weekday_label.text = "Week %d, day %d" % [week, day]
+
+
+func _on_x_pressed():
+	pass # Replace with function body.
